0eaf33464f0e429f92909cd39507e511a3ea7612
josdic1 <joshuadicker@gmail.com>
2026-02-09 16:37:07 -0500

    remove from main

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes"
 import { AuthProvider } from "./providers/AuthProvider";
 import { DataProvider } from "./providers/DataProvider";
-import { ThemeProvider } from "./providers/ThemeProvider";
 import "./index.css";
 
 const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <ThemeProvider>
       <DataProvider>
         <RouterProvider router={router} />
       </DataProvider>
-    </ThemeProvider>
   </AuthProvider>,
 );
